b1cac41a8bc47d814df51fd99cce3bdbc489de7c
hadik9595 <hadik9595@HRH-IC0027323.wrdsb.ca>
2015-09-28 14:23:34 -0400

    Beginning of assignment 3.

---
+++ src/A3Q1.java
@@ -0,0 +1,29 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadik9595
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        City kw = new City();
+        Robot karel = new Robot(kw,1,1, Direction.EAST);
+        new Wall(kw,1,5, Direction.WEST);
+        new Thing(kw,1,3);
+    }
+}
