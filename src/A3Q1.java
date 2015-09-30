cdf88c26144cf2f78ad84e080b8be9e40a771f61
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-09-30 13:47:27 -0400

    A3 Q1 done

---
+++ src/A3Q1.java
@@ -0,0 +1,65 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author watsk8668
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //make city
+        City kw = new City();
+        
+        //make robot
+        Robot karel = new Robot(kw, 0,0, Direction.EAST);
+        
+        //make things
+        new Thing(kw, 0,2);
+        new Thing(kw, 0,3); 
+        
+        //make wall
+        new Wall(kw, 0,4, Direction.WEST);
+        
+        //make karel stop at the first thing or wall
+        while (true)
+            if(karel.canPickThing()){
+                break;
+            } else if(karel.frontIsClear() == false) {
+                break;
+            } else if(karel.frontIsClear()) {
+                karel.move();
+            }
+        //make another robot
+        Robot tina = new Robot(kw, 3,0, Direction.EAST);
+        
+        //make wall
+        new Wall(kw, 3,4, Direction.EAST);
+        
+        //make things
+        new Thing(kw, 3,5);
+        
+        //make tina stop at the first thing or wall
+        while (true)
+            if(tina.canPickThing()){
+                break;
+            } else if(tina.frontIsClear() == false) {
+                break;
+            } else if(tina.frontIsClear()) {
+                tina.move();
+            }
+        
+    }
+}
