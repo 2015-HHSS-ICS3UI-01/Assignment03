f33aefb02218a48447319a13bcda8d9641c02e14
Administrator <Administrator@n-9c4c592af9fe4>
2015-09-29 20:13:44 -0400

    Looped, and executed well! Does anyone read this?

--- src/A3Q2.java
+++ src/A3Q2.java
@@ -9,7 +9,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author hadik9595
@@ -22,44 +21,69 @@ public class A3Q2 {
     public static void main(String[] args) {
         // TODO code application logic here
         City kw = new City();
-        Robot karel = new Robot(kw,1,1, Direction.EAST);
-        new Wall(kw,1,1, Direction.NORTH);
-        new Wall(kw,1,2, Direction.NORTH);
-        new Wall(kw,1,3, Direction.NORTH);
-        new Wall(kw,1,4, Direction.NORTH);
-        new Wall(kw,1,1, Direction.WEST);
-        new Wall(kw,2,1, Direction.WEST);
-        new Wall(kw,3,1, Direction.WEST);
-        new Wall(kw,3,1, Direction.SOUTH);
-        new Wall(kw,3,2, Direction.SOUTH);
-        new Wall(kw,3,3, Direction.SOUTH);
-        new Wall(kw,3,4, Direction.SOUTH);
-        new Wall(kw,3,4, Direction.EAST);
-        new Wall(kw,1,4, Direction.EAST);
-        new Wall(kw,2,4, Direction.EAST);
-        new Thing(kw,1,2);
-        new Thing(kw,2,2);
-        new Thing(kw,2,3);
-        new Thing(kw,2,4);
-        new Thing(kw,3,1);
-        new Thing(kw,3,4);
-        
-        while(true){
-            if(karel.frontIsClear()){
+        Robot karel = new Robot(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 3, 1, Direction.WEST);
+        new Wall(kw, 3, 1, Direction.SOUTH);
+        new Wall(kw, 3, 2, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.SOUTH);
+        new Wall(kw, 3, 4, Direction.SOUTH);
+        new Wall(kw, 3, 4, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.EAST);
+        new Wall(kw, 2, 4, Direction.EAST);
+        new Thing(kw, 1, 2);
+        new Thing(kw, 2, 2);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 2, 4);
+        new Thing(kw, 3, 1);
+        new Thing(kw, 3, 4);
+
+        while (true) {
+            if (karel.canPickThing()) {
+                karel.pickThing();
+            } else if (karel.frontIsClear()) {
                 karel.move();
-                if(karel.canPickThing()){
-                    karel.pickThing();
-                }
-                  
-                    
-                    
-                
-                
+
+            } else if (!karel.frontIsClear() && (karel.getDirection() == Direction.EAST)) {
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
             }
+            if (karel.getDirection() == Direction.SOUTH && (karel.frontIsClear())) {
+                karel.move();
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+
+            }else if(karel.getDirection() == Direction.WEST && (!karel.frontIsClear())){
+                karel.turnLeft();
+                karel.move();
+                karel.turnLeft();
+            }else if(karel.getDirection() == Direction.SOUTH && (!karel.frontIsClear())){
+                karel.turnLeft();
+                karel.turnLeft();
+                break;
                 
+                }
+            }
+            while(true){
+                    if(karel.getStreet() > 1){
+                karel.move();
+                
+            }else if(!karel.frontIsClear()){
+                    
+                    karel.turnLeft();
+                    }else if(karel.getAvenue() > 1){
+                        karel.move();
+                        
+                    }
+
         }
-        
+
     }
 }
-
-
