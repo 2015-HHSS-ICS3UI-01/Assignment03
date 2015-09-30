6b201857948b15e7de74909e4f09f4b722569963
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-09-30 14:24:10 -0400

    Assignment03

--- src/A3Q2.java
+++ src/A3Q2.java
@@ -29,11 +29,11 @@ public class A3Q2 {
         new Wall (eep, 1, 2, Direction.NORTH);
         new Wall (eep, 1, 3, Direction.NORTH);
         new Wall (eep, 1, 4, Direction.NORTH);
-        new Wall (eep, 1, 5, Direction.NORTH);
-        new Wall (eep, 1, 5, Direction.EAST);
-        new Wall (eep, 2, 5, Direction.EAST);
-        new Wall (eep, 3, 5, Direction.EAST);
-        new Wall (eep, 3, 5, Direction.SOUTH);
+        
+        new Wall (eep, 1, 4, Direction.EAST);
+        new Wall (eep, 2, 4, Direction.EAST);
+        new Wall (eep, 3, 4, Direction.EAST);
+        
         new Wall (eep, 3, 4, Direction.SOUTH); 
         new Wall (eep, 3, 3, Direction.SOUTH);
         new Wall (eep, 3, 2, Direction.SOUTH);
@@ -42,8 +42,43 @@ public class A3Q2 {
         new Wall (eep, 2, 1, Direction.WEST);
         
         //make litter
-        
+        new Thing (eep, 1, 2);
+        new Thing (eep, 2, 2);
+        new Thing (eep, 2, 3);
+        new Thing (eep, 2, 4);
+        new Thing (eep, 3, 1);
+        new Thing (eep, 3, 4);
         //make robot
         Robot jimbo = new Robot (eep, 1, 1, Direction.EAST);
-    }
-}
+        
+        //picking up litter
+        while (true){
+            if (jimbo.frontIsClear()){
+                while (jimbo.frontIsClear()){
+                    jimbo.move();
+                        if (jimbo.canPickThing()){
+                            jimbo.pickThing();
+                        }
+                                }
+                            }else if (!jimbo.frontIsClear()){
+                                for (int turns = 0; turns <3; turns = turns +1){
+                                    jimbo.turnLeft();
+                                }jimbo.move();
+                                    for (int turns = 0; turns <3; turns = turns +1){
+                                    jimbo.turnLeft();
+                            }
+        }}}}
+           
+
+            
+           
+                    
+                        
+                
+                
+            
+        
+          
+        
+    
+
