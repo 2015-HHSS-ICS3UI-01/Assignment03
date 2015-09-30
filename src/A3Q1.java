57aaa849af9598648a06fd9c0ddd70d28800be02
mitrm7692 <mitrm7692@HRH-IC0027327.wrdsb.ca>
2015-09-30 13:50:19 -0400

    A3Q1

---
+++ src/A3Q1.java
@@ -0,0 +1,48 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mitrm7692
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Make a City
+        City kw=new City();
+        //Make a Robot
+        Robot karel=new Robot(kw,0,0,Direction.EAST);
+        //Make a Wall
+        new Wall(kw,0,4,Direction.WEST);
+        //Make a Thing
+        new Thing (kw,0,3);
+        new Thing (kw,0,4);
+        while(karel.frontIsClear()){
+            karel.move();
+        if (karel.canPickThing()){
+            break;
+        }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
