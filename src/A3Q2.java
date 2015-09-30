e0f27abd923ba202411d559169ac286670b48d89
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-09-30 14:22:20 -0400

    School Changes 2

--- src/A3Q2.java
+++ src/A3Q2.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -23,16 +24,50 @@ public class A3Q2 {
         City kw = new City();
         
         // make robot to live in city
-        Robot karel = new Robot(kw, 0, 0, Direction.EAST);
+        Robot karel = new Robot(kw, 1, 1, Direction.EAST);
         
         // make walls within city
-        new Wall (kw, 0, 0, Direction.NORTH);
-        new Wall (kw, 0, 0, Direction.WEST);
-        new Wall (kw, 0, 1, Direction.NORTH);
-        new Wall (kw, 0, 2, Direction.NORTH);
-        new Wall (kw, 0, 3, Direction.NORTH);
-        new Wall (kw, 1, 0, Direction.WEST);
-        new Wall (kw, 2, 0, Direction.WEST);
-        new Wall (kw, 2, 0, Direction.SOUTH);
+        new Wall (kw, 1, 1, Direction.NORTH);
+        new Wall (kw, 1, 1, Direction.WEST);
+        new Wall (kw, 1, 2, Direction.NORTH);
+        new Wall (kw, 1, 3, Direction.NORTH);
+        new Wall (kw, 1, 4, Direction.NORTH);
+        new Wall (kw, 2, 1, Direction.WEST);
+        new Wall (kw, 3, 1, Direction.WEST);
+        new Wall (kw, 3, 1, Direction.SOUTH);
+        new Wall (kw, 3, 2, Direction.SOUTH);
+        new Wall (kw, 3, 3, Direction.SOUTH);
+        new Wall (kw, 3, 4, Direction.SOUTH);
+        new Wall (kw, 3, 4, Direction.EAST);
+        new Wall (kw, 2, 4, Direction.EAST);
+        new Wall (kw, 1, 4, Direction.EAST);
+        
+        // make things within the city for the robot to pick up and put down
+        new Thing (kw, 1, 2);
+        new Thing (kw, 2, 2);
+        new Thing (kw, 3, 1);
+        new Thing (kw, 1, 2);
+        new Thing (kw, 2, 3);
+        new Thing (kw, 2, 4);
+        new Thing (kw, 3, 4);
+        
+        karel.move();
+        while(karel.getAvenue() != 1){
+            while(karel.getDirection() == Direction.EAST || karel.getDirection() == Direction.WEST){
+                if(karel.frontIsClear()){
+                karel.move();
+                }else{
+                    karel.turnLeft();
+                    karel.turnLeft();
+                    karel.turnLeft();
+                }
+            }      
+            while(karel.getDirection() == Direction.SOUTH || karel.getDirection() == Direction.NORTH){
+                karel.move();
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+            }
+        }
     }
 }
