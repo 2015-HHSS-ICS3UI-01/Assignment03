398d4d485abe83c392d9c160877880f4bb1e726e
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-10-02 14:07:55 -0400

    Assignment03

---
+++ src/A3Q5.java
@@ -0,0 +1,25 @@
+
+import becker.robots.City;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sevcm7279
+ */
+public class A3Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // make city
+        City eep = new City();
+        
+        //boxes
+        new Wall (eep, 1, 1, Direction. )
+    }
+}
