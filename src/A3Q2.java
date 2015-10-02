657dc8e592c640ca16c49621af9c311427abd4fe
mitrm7692 <mitrm7692@HRH-IC0027327.wrdsb.ca>
2015-10-02 13:56:10 -0400

    A3Q2

---
+++ src/A3Q2.java
@@ -0,0 +1,83 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mitrm7692
+ */
+public class A3Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Make a City
+        City kw = new City();
+        //Make a new Robot
+        Robot karel = new Robot(kw, 1, 1, Direction.EAST);
+        //Make Walls 
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 3, 1, Direction.WEST);
+        new Wall(kw, 3, 1, Direction.SOUTH);
+        new Wall(kw, 3, 2, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.SOUTH);
+        new Wall(kw, 3, 4, Direction.SOUTH);
+        new Wall(kw, 3, 4, Direction.EAST);
+        new Wall(kw, 2, 4, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.NORTH);
+        //Make Things
+        new Thing(kw, 1, 2);
+        new Thing(kw, 2, 2);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 3, 1);
+        new Thing(kw, 3, 4);
+        new Thing(kw, 2, 4);
+        //Make karel move
+
+
+        while (true) {
+            while (karel.frontIsClear()) {
+                karel.move();
+                if (karel.canPickThing()) {
+                    karel.pickThing();
+                }
+            }
+            karel.turnLeft();
+            karel.turnLeft();
+            while (karel.frontIsClear()) {
+                karel.move();
+            }
+            karel.turnLeft();
+            if (!karel.frontIsClear()){
+            karel.turnLeft();
+            karel.turnLeft();
+            while(karel.frontIsClear())
+                karel.move();
+           if(!karel.frontIsClear())
+               break;
+            }
+            karel.move();
+            karel.turnLeft();
+            if(karel.canPickThing()){
+                karel.pickThing();
+            }                     
+            
+    }
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+    }
+}
