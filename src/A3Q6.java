9d98e46cf2907b7786a683c24fb0ae35742da585
Matt <Matt@AllegaeonV2>
2015-10-02 12:14:30 -0400

    Created A3Q1, completed. created A3Q2 Created A3Q3, completed Created A3Q4, completed Created A3Q5, completed Created A3Q6, completed

---
+++ src/A3Q6.java
@@ -0,0 +1,77 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Matt
+ */
+public class A3Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        City kw = new City();
+
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.EAST);
+        new Wall(kw, 2, 2, Direction.EAST);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        new Wall(kw, 2, 1, Direction.SOUTH);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 1, 1, Direction.WEST);
+
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.EAST);
+        new Wall(kw, 2, 5, Direction.EAST);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.WEST);
+        new Wall(kw, 1, 4, Direction.WEST);
+
+        new Wall(kw, 4, 1, Direction.NORTH);
+        new Wall(kw, 4, 2, Direction.NORTH);
+        new Wall(kw, 4, 2, Direction.EAST);
+        new Wall(kw, 5, 2, Direction.EAST);
+        new Wall(kw, 5, 2, Direction.SOUTH);
+        new Wall(kw, 5, 1, Direction.SOUTH);
+        new Wall(kw, 5, 1, Direction.WEST);
+        new Wall(kw, 4, 1, Direction.WEST);
+
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.EAST);
+        new Wall(kw, 5, 5, Direction.EAST);
+        new Wall(kw, 5, 5, Direction.SOUTH);
+        new Wall(kw, 5, 4, Direction.SOUTH);
+        new Wall(kw, 5, 4, Direction.WEST);
+        new Wall(kw, 4, 4, Direction.WEST);
+
+        Robot matt = new Robot(kw, 3, 3, Direction.SOUTH);
+
+        for (int allSquares = 4; allSquares >0;allSquares--){
+            for (int squareRed = 3; squareRed > 0; squareRed--) {
+                matt.move();
+                matt.move();
+                matt.move();
+                matt.turnLeft();
+
+            }
+            matt.move();
+            matt.move();
+            matt.move();
+
+        } 
+    }
+}
+
