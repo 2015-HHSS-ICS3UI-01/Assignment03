20edecf58bdcf450d946f69f7d913e483f0a2385
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-10-06 14:23:24 -0400

    assaigment 5

---
+++ src/a3q6.java
@@ -0,0 +1,26 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lalim9800
+ */
+public class a3q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        City kw= new City();
+        
+        
+       
+    }
+}
