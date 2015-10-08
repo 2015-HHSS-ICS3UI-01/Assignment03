ab21fd0aa538ffb2fa7eb850acb170d4802dd9bd
lalim9800 <lalim9800@HRH-IC0027320.wrdsb.ca>
2015-10-08 13:42:57 -0400

    a1q5

--- src/a3q5.java
+++ src/a3q5.java
@@ -1,5 +1,8 @@
 
 import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
 
 /*
  * To change this template, choose Tools | Templates
@@ -19,6 +22,54 @@ public class a3q5 {
         
        //make city
         City kw= new City();
+        //make robot 
+        Robot lali=new Robot(kw,2,2,Direction.EAST);
+             
+               //make things
+        
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             new Thing(kw,2,2);
+             //set int backpack to 0 
+            int Backpack=0;
+            
+             //Robot moves and pickthigns up and puts it down,adding 1 to the
+            //backpack each time untill the value of backpack is 10 
+             while(true){
+                 lali.pickThing();
+                 lali.move();
+                 lali.putThing();
+                 Backpack++;
+                 if(Backpack==10){
+                     break;
+                     
+
+                 //make robot turn around 
+             
+   
+             } lali.turnLeft();
+             lali.turnLeft();
+             lali.move();
+             lali.turnLeft();
+             lali.turnLeft();
+             
+             
+             } 
+            
+                    
+            
+             
+             
+             
+             
+                
         
         
         
