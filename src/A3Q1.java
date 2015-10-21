83e53a698f41c3c5cb76334777e2a2278c7fec8e
agott2059 <agott2059@HRH-IC0027338.wrdsb.ca>
2015-10-21 12:48:42 -0400

    A3

--- src/A3Q1.java
+++ src/A3Q1.java
@@ -20,27 +20,25 @@ public class A3Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // create city, robot, walls and things
-        
+
         City thomas = new City();
-        
-        Robot tom = new Robot (thomas, 1,1, Direction.EAST);
-        
-        new Thing(thomas, 1,3);
-        new Thing(thomas, 1,4);
-        new Wall(thomas,1,5, Direction.WEST);
-        
-//move forward if front is clear but stop when thing is found
-        while(tom.frontIsClear())
-        {
+
+        Robot tom = new Robot(thomas, 1, 1, Direction.EAST);
+
+        new Thing(thomas, 1, 3);
+        new Thing(thomas, 1, 4);
+        new Wall(thomas, 1, 5, Direction.WEST);
+
+        //move forward if front is clear but stop when thing is found
+        while (tom.frontIsClear()) {
             tom.move();
             //if tom can pick thing, thing is found, so end loop
-        if(tom.canPickThing())
-        {
-            break;
+            if (tom.canPickThing()) {
+                break;
+            }
         }
-        }
-                    
+
     }
 }
